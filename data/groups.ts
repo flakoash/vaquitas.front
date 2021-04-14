124435e11dd8b36739cefcbfacfdc2844a2bbe23
flakoash <flash.ar79@gmail.com>
2021-04-14 11:00:03 -0400

    dummy data to test UI

---
+++ data/groups.ts
@@ -0,0 +1,114 @@
+export default [
+  {
+    id: "1",
+    name: "Group 1",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: -230,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+  {
+    id: "2",
+    name: "Group 2",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: 120,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+  {
+    id: "3",
+    name: "Group 3",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: 0,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+  {
+    id: "4",
+    name: "Group 4",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: -10,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+  {
+    id: "5",
+    name: "Group 5",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: 0,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+  {
+    id: "6",
+    name: "Group 6",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: -73,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+  {
+    id: "7",
+    name: "Group 7",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: 41,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+  {
+    id: "8",
+    name: "Group 8",
+    members: [
+      { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
+    ],
+    icon: "https://picsum.photos/50",
+    balance: {
+      id: "balance_id",
+      value: 12,
+      to: null,
+      createdAt: 1617475021,
+    },
+  },
+];
