b43a86adfd37a12ca08dbb685b74a0ad2e348541
flakoash <flash.ar79@gmail.com>
2021-04-14 10:58:36 -0400

    feat: changed screens to use backend

--- screens/GroupScreen.tsx
+++ screens/GroupScreen.tsx
@@ -1,41 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRoute } from "@react-navigation/native";
-import { FlatList } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
 import GroupTransaction from "../components/GroupTransaction";
-
-const getTransactionsData = () => {
-  return [
-    {
-      id: "tr_id",
-      title: "pago 1",
-      description: "pago por algo",
-      amount: 100,
-      creator: { id: "1", name: "User 1" },
-      createdAt: 1617475021,
-      attachment: "some attachment..",
-    },
-    {
-      id: "tr_id2",
-      title: "pago 2",
-      description: "pago por otro",
-      amount: 100,
-      creator: { id: "2", name: "User 2" },
-      createdAt: 1617277021,
-      attachment: "some attachment..",
-    },
-  ];
-};
+import TotalBalance from "../components/TotalBalance";
+import useAsyncStorage from "../hooks/useAsyncStorage";
+import { User } from "../types";
+import Colors from "../constants/Colors";
+import { Animated } from "react-native";
 
 const GroupScreen = () => {
   const route = useRoute();
-  const trData = getTransactionsData();
-  console.warn(route.params);
+  const [data, setData] = useState();
+  const { id, name } = route.params;
+
+  const getTransactionsData = () => {
+    fetch("http://192.168.56.1:8080/api/transaction?groupId=" + id)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setData(responseJson);
+      });
+  };
+  getTransactionsData();
+
+  const summary = getSummary(id);
+
   return (
-    <FlatList
-      data={trData}
-      renderItem={({ item }) => <GroupTransaction transaction={item} />}
-    />
+    <View style={styles.container}>
+      <Animated.View style={{ width: "100%" }}>
+        <TotalBalance
+          summary={summary}
+          unit="friend"
+          prepositions={{ lent: "to", borrowed: "from", settleUp: "with" }}
+        ></TotalBalance>
+      </Animated.View>
+      <Animated.FlatList
+        data={data}
+        renderItem={({ item }) => <GroupTransaction transaction={item} />}
+      />
+    </View>
   );
 };
+
+const getSummary = (group_id: string) => {
+  return {
+    borrowed: {
+      amount: -10,
+      len: 2,
+    },
+    lent: {
+      amount: 10,
+      len: 2,
+    },
+    settleUp: {
+      amount: 10,
+      len: 1,
+    },
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.light.base,
+  },
+});
 
 export default GroupScreen;
--- screens/GroupsListScreen.tsx
+++ screens/GroupsListScreen.tsx
@@ -1,63 +1,59 @@
-import * as React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Animated, FlatList, StyleSheet } from "react-native";
 
 import GroupListItem from "../components/GroupListItem";
 import { Text, View } from "../components/Themed";
-import { Group } from "../types";
-
-const getGroupsData = () => {
-  const groups: Group[] = [
-    {
-      id: "1",
-      name: "Group 1",
-      members: [
-        { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
-      ],
-      icon: "https://picsum.photos/50",
-      balance: {
-        id: "balance_id",
-        value: -100,
-        to: null,
-        createdAt: 1617475021,
-      },
-    },
-    {
-      id: "2",
-      name: "Group 2",
-      members: [
-        { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
-      ],
-      icon: "https://picsum.photos/50",
-      balance: {
-        id: "balance_id",
-        value: 100,
-        to: null,
-        createdAt: 1617475021,
-      },
-    },
-    {
-      id: "3",
-      name: "Group 3",
-      members: [
-        { id: "user_id", name: "User 1", photo: "https://i.pravatar.cc/50" },
-      ],
-      icon: "https://picsum.photos/50",
-      balance: {
-        id: "balance_id",
-        value: 0,
-        to: null,
-        createdAt: 1617475021,
-      },
-    },
-  ];
-  return groups;
-};
+import TotalBalance from "../components/TotalBalance";
+import Colors from "../constants/Colors";
+import groupsData from "../data/groups";
+import useAsyncStorage from "../hooks/useAsyncStorage";
+import { Group, User } from "../types";
 
 export default function TabOneScreen() {
-  const groups = getGroupsData();
+  const [groups, setGroups] = useState<Group[]>();
+  const [storageUser, , , isUserLoaded] = useAsyncStorage("user_id");
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    if (isUserLoaded) setCurrentUser(JSON.parse(storageUser as string));
+  }, [isUserLoaded]);
+  const [summary, setSummary] = useState({
+    borrowed: {
+      amount: 0,
+      len: 0,
+    },
+    lent: {
+      amount: 0,
+      len: 0,
+    },
+    settleUp: {
+      amount: 0,
+      len: 0,
+    },
+  });
+  const getGroupsData = () => {
+    if (currentUser !== null)
+      fetch("http://192.168.56.1:8080/api/group?userId=" + currentUser.id)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          setGroups(responseJson);
+        })
+        .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getGroupsData();
+  }, []);
+  useEffect(() => {
+    if (groups !== undefined) setSummary(getSummary(groups));
+  }, [groups]);
+
   return (
     <View style={styles.container}>
-      <FlatList
+      <Animated.View style={{ width: "100%" }}>
+        <TotalBalance summary={summary} />
+      </Animated.View>
+
+      <Animated.FlatList
         style={{ width: "100%" }}
         data={groups}
         renderItem={({ item }) => <GroupListItem group={item} />}
@@ -67,10 +63,38 @@ export default function TabOneScreen() {
   );
 }
 
+const getSummary = (groups: Group[]) => {
+  const borrowed: Group[] = groups.filter((value) => {
+    return value.balance < 0;
+  });
+  const lent: Group[] = groups.filter((value) => {
+    return value.balance > 0;
+  });
+  const settleUp: Group[] = groups.filter((value) => {
+    return value.balance == 0;
+  });
+
+  return {
+    borrowed: {
+      amount: borrowed.reduce((a, b) => a + b.balance, 0),
+      len: borrowed.length,
+    },
+    lent: {
+      amount: lent.reduce((a, b) => a + b.balance, 0),
+      len: lent.length,
+    },
+    settleUp: {
+      amount: settleUp.reduce((a, b) => a + b.balance, 0),
+      len: settleUp.length,
+    },
+  };
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: Colors.light.base,
   },
 });
