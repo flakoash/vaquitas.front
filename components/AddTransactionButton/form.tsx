c10cd263ecb22c9865d2dadad7e36273347c644e
flakoash <flash.ar79@gmail.com>
2021-05-26 15:34:45 -0400

    sort members so that current user is the first in the array

--- components/AddTransactionButton/form.tsx
+++ components/AddTransactionButton/form.tsx
@@ -25,6 +25,21 @@ const AddTransactionForm = (props: AddTransaciontFormProps) => {
     username: null,
     token: null,
   });
+  const [sortedMembers, setSortedMembers] = useState(null);
+
+  // sort members so curent user is the first one
+  useEffect(() => {
+    if (members !== null && currentUser.id !== null) {
+      const user = members.filter(
+        (member: User) => member.id === currentUser.id
+      )[0];
+      const otherMembers = members.filter(
+        (member: User) => member.id !== currentUser.id
+      );
+      otherMembers.splice(0, 0, user);
+      setSortedMembers(otherMembers);
+    }
+  }, [members, currentUser]);
 
   useEffect(() => {
     if (isUserLoaded) setCurrentUser(JSON.parse(storageUser as string));
@@ -53,7 +68,7 @@ const AddTransactionForm = (props: AddTransaciontFormProps) => {
 
   let zip = (a1: any, a2: any) => a1.map((x: any, i) => [x, a2[i]]);
 
-  const getSplitterAmounts = () => {
+  const getSplittedAmounts = () => {
     const n = members.length;
     const splittedAmmount =
       Math.round((amount / n + Number.EPSILON) * 100) / 100;
@@ -77,7 +92,7 @@ const AddTransactionForm = (props: AddTransaciontFormProps) => {
       >
         <Image source={{ uri: member.photo as string }} style={styles.avatar} />
         <Text style={styles.groupMemberText}>
-          {member.id === currentUser.id ? "You" : member.name}
+          {member.id === currentUser.id ? "Me" : member.name}
         </Text>
         <Input
           name={"Group_Member_" + member.id.toString()}
@@ -91,13 +106,14 @@ const AddTransactionForm = (props: AddTransaciontFormProps) => {
     );
   };
   const splits = () => {
-    const amounts = getSplitterAmounts();
-    const zippedMembers = zip(members, amounts);
+    const amounts = getSplittedAmounts();
+    const zippedMembers = zip(sortedMembers, amounts);
     return (
       <View style={styles.splitContainer}>
-        {zippedMembers.map(([member, amount]: [User, number]) => {
-          return split(member, amount);
-        })}
+        {sortedMembers !== null &&
+          zippedMembers.map(([member, amount]: [User, number]) => {
+            return split(member, amount);
+          })}
       </View>
     );
   };
@@ -110,7 +126,7 @@ const AddTransactionForm = (props: AddTransaciontFormProps) => {
 
     let involved: any[] = [];
     if (form["splitEqual"]) {
-      const amounts = getSplitterAmounts();
+      const amounts = getSplittedAmounts();
       const zippedMembers = zip(members, amounts);
 
       involved = zippedMembers.map(([member, amount]: [User, number]) => {
